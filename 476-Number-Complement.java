e1f6627bebf91f19d0a64c4b9d4dc3bf82dcb7ab
Pranjal Piyush <pranjalpiyush29@gmail.com>
2026-03-11 23:50:53 +0530

    same as 1007 one but numbers start from 1

---
+++ 476-Number-Complement.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findComplement(int num) {
+        //check the bits
+        StringBuilder s=new StringBuilder();
+        while(num>0){
+            int temp=num%2;
+            if(temp==1){
+                s.append("0");
+            }
+            else{
+                s.append("1");
+            }
+            num/=2; //can be done using shift operations
+        }
+        //then use the 2 to the power sum to calculate to remove leading zero problem
+        String str=s.reverse().toString();
+        int sum=0;
+        for(int i=s.length()-1;i>=0;i--){
+            int temp=Character.getNumericValue(s.charAt(i));
+            sum+=(int) (temp*Math.pow(2,s.length()-1-i));
+        }
+        return sum;
+    }
+}
